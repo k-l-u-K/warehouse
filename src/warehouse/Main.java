0a3fc5deb26c7713c4929d8c02a9fc0d7985173d
pascalparu <pascal@parussudis.de>
2015-01-17 16:34:51 +0100

    Update Main.java

--- src/warehouse/Main.java
+++ src/warehouse/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 		File file = new File(".\\data\\DateiZumEinlesen.txt");
 		OutputStream ostream = new FileOutputStream(file);
-		PrintWriter writer = new PrintWriter(ostream);
+		PrintWriter writer = new PrintWriter(ostream);//change
 		Random random = new Random();
 		Scanner reader = new Scanner(System.in);
 		System.out.print("Eingabe > ");

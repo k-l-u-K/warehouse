a580cd1e67d9ab1c30cecf1ea62263c15cf3ff5b
Robsond <robsondeluxe@web.de>
2015-02-10 12:03:33 +0100

    Update Part.java

--- src/warehouse/Part.java
+++ src/warehouse/Part.java
@@ -1,6 +1,8 @@
 package warehouse;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 //Klasse für die Teile, welche einsortiert werden sollen
@@ -9,7 +11,7 @@ public class Part {
 	private String description;
 	private int partnumber;
 	private int amount;
-	private int size;
+	private static int size;
 	private static List<Part> PartList = new ArrayList<Part>();
 
 	
@@ -22,8 +24,28 @@ public class Part {
 		PartList.add(this);
 	}
 
+	public static int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
 	public void delPart() {
 		PartList.remove(this);
 	}
-
+	
+	public static List<Part> getTeilListe() {
+		return Collections.unmodifiableList(PartList);
+	}
+	/*
+	public static List<Part> createPartList(int regalnr, int x, int z, Part part) {
+		LinkedList<Part> tempList = new LinkedList<Part>();
+		for (Part tempTeil: PartList) {
+			if (tempTeil.equals(part))
+					tempList.add(tempTeil);
+		}
+		return Collections.unmodifiableList(tempList);
+	}*/
 }

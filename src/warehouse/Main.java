555c6f7a1c2f1e2b37b178ed428f235595e82d5b
Robsond <robsondeluxe@web.de>
2015-02-13 17:25:08 +0100

    ToDo Update

--- src/warehouse/Main.java
+++ src/warehouse/Main.java
@@ -1,19 +1,16 @@
 package warehouse;
 
-import java.io.*;
-
 public class Main {
 
 	/*
 	 * ToDo:
-	 * Infofeld mit Gesamtanzahl eines Teils mit gleichem Namen
 	 * freien Platz (Kapazität) und Anzahl ausgeben (Anzahl der leeren Fächer vllt mit der Fach suche machen und 
 	 * 	ein Fach für ein Objekt der größe 10 suchen?
 	 * 		Flo: Die Fachsuche bricht aber ab, wenn sie ein Fach gefunden hat, 
 	 * 		aber die Schleife ansich müsste man verwenden können ju.)
-	 * korrekte Regalnr.-/Fach-Anzeige
-	 * Beim zufälligen Befüllen werden IDs übersprungen, die man nachträglich auch nicht nutzen kann
+	 * korrekte Fach-Anzeige
 	 * Daten in Datei schreiben / lesen
+	 * Anlegen eines Teils, welches größer 10 ist --> andere Fehlermeldung
 
 	 * ToDo für Nacharbeiten:
 	 * alle Variablen auf Korrektheit von private / public kontrollieren
@@ -26,14 +23,8 @@ public class Main {
 	 * Exceptionsbehandlung ggf. überarbeiten
 	 */
 
-	public static void main(String[] args) throws FileNotFoundException {
-		File file = new File(".\\data\\DateiZumEinlesen.txt");
-		OutputStream ostream = new FileOutputStream(file);
-		PrintWriter writer = new PrintWriter(ostream);
-
-		//new Warehouse();
+	public static void main(String[] args) {
 		new TransportVehicle(0, 0, 0);
-		
 		new MainFrame();
 		//Beim Aufruf des zufälligen Einordnens kann als Parameter eine Zahl (sinnvoll größer 5 ;-)) übergeben werden
 		//damit jedes Fach im Lager mit einem Teil befüllt wird -> Lager voll kann getestet werden

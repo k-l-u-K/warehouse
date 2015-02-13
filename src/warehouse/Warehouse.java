33d4b10ebd2e454493468b8ed8d270ee403bdbfa
McFlori <florian1592@gmx.de>
2015-02-13 18:48:45 +0100

    Fix: Teilgröße
    Teile konnten größer als 10 sein, was in der Ausgabe Lager voll endete

--- src/warehouse/Warehouse.java
+++ src/warehouse/Warehouse.java
@@ -51,19 +51,18 @@ public class Warehouse implements Serializable {
 
 	// Lagert ein Teil ein
 	public static String teilEinlagern(Part part, Compartment compartment) {
-		int partSize = part.getSize();
-		if (partSize <= 0)
-			return "Größe muss größer null sein!";
+		if (part.getSize() <= 0 || part.getSize() > 10)
+			return "Größe muss zwischen 1 und 10 GE betragen!";
 		if (compartment == null) 
 			return "Lager voll!";
 		// wenn noch Platz, dann einlagern
-		if ((compartment.getCapacity() - partSize) >= 0) {
+		if ((compartment.getCapacity() - part.getSize()) >= 0) {
 			//Fahrzeug kann hier mit dem Teil zum Zielort fahren
 			TransportVehicle.driveToCompartment(part, compartment);
 			// einlagern
 			compartment.getPartList().add(part);
 			// Kapazität verringern
-			compartment.setCapacity(compartment.getCapacity() - partSize);
+			compartment.setCapacity(compartment.getCapacity() - part.getSize());
 			//Zeile hinzufügen
 			MainFrame.addARow(part, compartment);
 			//Teil der Anzahlliste hinzufügen

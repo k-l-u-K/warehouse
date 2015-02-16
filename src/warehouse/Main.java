5c8764c5164645c76aeef3b2d2c071a229e3c3ed
Robsond <robsondeluxe@web.de>
2015-02-16 10:30:13 +0100

    Update ToDo

--- src/warehouse/Main.java
+++ src/warehouse/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	/*
 	 * ToDo für Nacharbeiten:
 	 * Buttonverteilung & Layout
+	 * wenn Lageroption mit Lager befüllen etc. als Menü --> Anzeige der freien Fächer verbuggt
 	 * beim Auslagern: wenn keine Teilenummer vorhanden -> Info an Benutzer 
 	 * sinnvoll kommentieren
 	 * Kurze Entwurfsbeschreibung (wenige Seiten)

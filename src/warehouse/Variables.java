542038e604ac8a6f3def3435dd152d79c751ab9d
Robsond <robsondeluxe@web.de>
2015-02-16 10:28:11 +0100

    neue Variablen

--- src/warehouse/Variables.java
+++ src/warehouse/Variables.java
@@ -2,10 +2,12 @@ package warehouse;
 
 public class Variables {
 	public static final int
-		REGALCOUNT = 8,
-		COMPARTMENTSIDEBYSIDE = 10,
-		COMPARTMENTONTOPOFEACHOTHER = 10,
-		RANDOM = 800,
-		FILLEVERYPARTWITHTHISSIZE = 10,
-		CAPACITY = 10;
+		REGALCOUNT = 8,						// Anz. der Regale
+		COMPARTMENTSIDEBYSIDE = 10,			// Anz. der Fächer nebeneinander
+		COMPARTMENTONTOPOFEACHOTHER = 10,	// Anz. der Fächer übereinander
+		RANDOM = 50,						// wenn FILLEVERYCOMPARTMENT = false steht das für die Anzahl der anzulegeneden Teile
+		CAPACITY = 10;						// Fachkapazität - bei Änderung muss die Datei neu angelegt werden (händisch)
+	public static final boolean
+		FILLEVERYCOMPARTMENT = false,		// füllt jedes Fach - dauert ggf. sehr lange!
+		MULTIPARTTYPPERCOMPARTMENT = false;	// gibt an, ob verschiedene Teile mit verschiedenen Namen in ein Fach eingeordnet werden dürfen
 }
